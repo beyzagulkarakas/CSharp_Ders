cb8f49a8e0952a7d913c9f51537bd7cbccef0cf7
beyzagulkarakas <karakasbeyza5@gmail.com>
2025-10-28 16:06:20 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region String Değişkenler
+            //string
+            //Değişken_türü değişken_adı;
+
+            //string name;
+            //name = "Beyza";
+            //Console.WriteLine(name);
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;  //aynı satırda aynı türden birden fazla değişken tanımlanabilir
+
+            //customerName = "Ali";
+            //customerSurname = "Çınar";
+            //customerPhone = "+90 500 400 30 20";
+            //customerEmail = "deneme@gmail.com";
+            //district = "Kadıköy";
+            //city = "İstanbul";
+
+            //Console.WriteLine("**** Rezervasyon Kartı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("Müşteri : " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim : " + customerPhone);
+            //Console.WriteLine("Email : " + customerEmail);
+            //Console.WriteLine("Adres : " + district + "/" + city);
+
+
+            #endregion
+
+            #region Int değişkenler
+
+            //int
+            //int number = 24;
+            ////Console.WriteLine(number);
+            //int hamburgerPrice = 300;
+            //int cokePrice = 35;
+            //int waterPrice = 10;
+            //int friesPrice = 50;
+            //int pizzaPrice = 250;
+            //int lemonadePrice = 30;
+
+            //Console.WriteLine("**** Restoran Menü Fiyatı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("-----Hamburger Fiyatı : " + hamburgerPrice + "TL");
+            //Console.WriteLine("-----Pizza : " + pizzaPrice + "TL");
+            //Console.WriteLine("-----Kola : " + cokePrice + "TL");
+            //Console.WriteLine("-----Limonata : " + lemonadePrice + "TL");
+            //Console.WriteLine("-----Kızartma : " + friesPrice + "TL");
+            //Console.WriteLine("-----Su : " + waterPrice + "TL");
+            //Console.WriteLine("**** Restoran Menü Fiyatı ****");
+
+            //Console.WriteLine();
+            //int hamburgerCount;
+            //int cokeCount;
+            //int waterCount;
+            //int friesCount;
+            //int pizzaCount;
+            //int lemonadeCount;
+
+            //int totalHamburgerPrice = 0;
+            //int totalCokePrice;
+            //int totalWaterPrice;
+            //int totalFriesPrice;
+            //int totalPizzaPrice;
+            //int totalLemonadePrice;
+
+            //hamburgerCount = 3;
+            //cokeCount = 3;
+            //waterCount = 3;
+            //friesCount = 1;
+            //pizzaCount = 0;
+            //lemonadeCount = 0;
+
+            //totalHamburgerPrice = hamburgerCount * hamburgerPrice;
+            //totalCokePrice = cokeCount * cokePrice;
+            //totalWaterPrice = waterCount * waterPrice;
+            //totalLemonadePrice = lemonadeCount * lemonadePrice;
+            //totalPizzaPrice = pizzaCount * pizzaPrice;
+            //totalWaterPrice = waterCount * waterPrice;
+
+
+            //Console.WriteLine("-------------------------------");
+            //Console.WriteLine("Hamburger Tutarı : " + totalHamburgerPrice + "TL");
+
+
+
+            #endregion
+        }
+    }
+}
